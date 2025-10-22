0c8099e5e892231c6d27fc2020092568f061a5ea
Ali Ubaid <ubaid.sgd321@gmail.com>
2025-10-22 11:36:34 +0500

    Modify files are changed

--- Backend/controllers/modifyClass_cont.js
+++ Backend/controllers/modifyClass_cont.js
@@ -1,6 +1,44 @@
+const { parse } = require("path");
 const lectures = require("../model/lectures");
 const roomNumber = require("../model/roomNumber");
 const room = require("../model/roomNumber");
+
+
+
+const addClass = async(req,res)=>{
+    const {data} = req.body;
+    let slot = parseFloat(data.duration)/30;
+
+    let timeToCompare = [];
+    for(let i=0; i<slot; i++){
+        timeToCompare.push(parseFloat(data.time) + 0.5 * i);
+    }
+    const lectFound = await lectures.find({roomNumber:data.roomNum,  time: { $in: timeToCompare },day:data.day});
+    if(lectFound.length>0){
+        return res.json({
+            status:"failed",
+            error:"Clash Found"
+        })
+    }
+    else{
+    const newLecture = lectures.create({
+        subjectName:data.subjectName,
+        teacherName:data.teacherName,
+        time:data.time,
+        roomNumber:data.roomNum,
+        day:data.day,
+        semester:data.semester,
+        type:data.type,
+        duration:data.duration,
+        slots:slot
+    })
+     res.json({
+                status:"success",
+                updated:true
+            });
+    }
+
+}
 
 
 const editClass = async(req,res)=>{
@@ -45,5 +83,15 @@ const editClass = async(req,res)=>{
 }
 
 
+const deleteClass = async(req,res)=>{
+    const {data} = req.body;
+    const lect = await lectures.findOneAndDelete({_id:data.lectureId});
+    console.log("Api Fetched");
+    
+    res.json({
+        status:"success"
+    })
+}
 
-module.exports = editClass;
+
+module.exports = {addClass,editClass,deleteClass};
